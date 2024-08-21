ebe6eb2dd442ba916e633f1f061c860a32d7d8a1
Srutik <81953254+Srutik@users.noreply.github.com>
2024-08-21 16:42:21 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "azurerm_role_definition" "custom_role" {
 }
 
 resource "azurerm_policy_definition" "custom_policy" {
-  name         = "CustomPolicy2"
+  name         = "CustomPolicy3"
   policy_type  = "Custom"
   mode         = "All"
   display_name = "Custom Policy"
@@ -45,7 +45,7 @@ resource "azurerm_policy_definition" "custom_policy" {
 }
 
 resource "azurerm_subscription_policy_assignment" "custom_policy_assignment" {
-  name                 = "customPolicyAssignment2"
+  name                 = "customPolicyAssignment3"
     policy_definition_id = azurerm_policy_definition.custom_policy.id
   subscription_id                = "/subscriptions/${var.subscription_id}"
 }
