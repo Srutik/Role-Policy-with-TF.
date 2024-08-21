d6ae5f884a28e904fdaa23bfc8ffcadf0790c01b
Srutik <81953254+Srutik@users.noreply.github.com>
2024-08-21 16:31:14 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_role_definition" "custom_role" {
-  name        = "CustomRole"
+  name        = "CustomStorageRole"
   description = "Custom role with limited permissions"
   scope       = "/subscriptions/${var.subscription_id}"
 
@@ -25,7 +25,7 @@ resource "azurerm_role_definition" "custom_role" {
 }
 
 resource "azurerm_policy_definition" "custom_policy" {
-  name         = "CustomPolicy1"
+  name         = "CustomPolicy2"
   policy_type  = "Custom"
   mode         = "All"
   display_name = "Custom Policy"
@@ -44,7 +44,7 @@ resource "azurerm_policy_definition" "custom_policy" {
 }
 
 resource "azurerm_subscription_policy_assignment" "custom_policy_assignment" {
-  name                 = "customPolicyAssignment"
+  name                 = "customPolicyAssignment2"
     policy_definition_id = azurerm_policy_definition.custom_policy.id
   subscription_id                = "/subscriptions/${var.subscription_id}"
 }
